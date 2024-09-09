d5fa7391bcfbb9395c2683f0f1c01f848aa48901
AsriFox <asrifox@yandex.ru>
2024-09-09 21:49:15 +0300

    Fix #1
    Also maybe fix XML-only blocks missing from tree view

--- src/moduleTree.ts
+++ src/moduleTree.ts
@@ -43,20 +43,27 @@ export class GNURadioModuleTreeDataProvider implements TreeDataProvider<TreeItem
             }
             return await getBlockFilesTree(element.label.toString(), Uri.file(this.cwd), this.moduleName);
         } else {
-            const cppBlocks = blocks.getCppBlocks(this.cwd, this.moduleName);
-            const xmlBlocks = blocks.getXmlBlocks(this.cwd, this.moduleName);
-            return Array.from(blocks.getAllBlocks(this.cwd, this.moduleName))
-                .map((name) => {
+            const addTreeItem = (name: string) => {
+                if (!tree.get(name)) {
                     let item = new TreeItem(name, TreeItemCollapsibleState.Collapsed);
                     item.contextValue = 'block';
-                    if (cppBlocks.includes(name)) {
-                        item.contextValue += '.cpp';
-                    }
-                    if (xmlBlocks.includes(name)) {
-                        item.contextValue += '.xml';
-                    }
-                    return item;
-                });
+                    tree.set(name, item);
+                }
+            };
+            let tree = new Map<string, TreeItem>();
+            blocks.getCppBlocks(this.cwd, this.moduleName).forEach(name => {
+                addTreeItem(name);
+                tree.get(name)!.contextValue += '.cpp';
+            });
+            blocks.getXmlBlocks(this.cwd, this.moduleName).forEach(name => {
+                addTreeItem(name);
+                tree.get(name)!.contextValue += '.xml';
+            });
+            blocks.getGrcBlocks(this.cwd, this.moduleName).forEach(addTreeItem);
+            blocks.getPyBlocks(this.cwd, this.moduleName).forEach(addTreeItem);
+            return Array.from(tree.entries())
+                .sort(([a, _a], [b, _b]) => a.localeCompare(b))
+                .map(([_name, item]) => item);
         }
     }
 }
@@ -85,33 +92,30 @@ export async function getBlockFilesTree(block: string, baseUri: Uri, moduleName:
 
     const grcFiles = (await readdir('grc'))
         .filter((value) =>
-            value[0].startsWith(`${moduleName}_${block}`) &&
-            (blocks.filterGrcBlocks(value[0]) || blocks.filterXmlBlocks(value[0])))
+            RegExp(`^${moduleName}_${block}\.(block\.yml|xml)$`).test(value[0]))
         .map(mapBlockToTreeItem('Block definition', ['grc']));
 
     const cppFiles = (await readdir('include', 'gnuradio', moduleName))
         .filter((value) =>
-            value[0].startsWith(block) &&
-            blocks.filterCppBlocks(value[0]))
+            blocks.filterCppBlocks(value[0]) &&
+            blocks.removeExt(value[0]) === block)
         .map(mapBlockToTreeItem('Public header', ['include', 'gnuradio', moduleName]));
 
     const pyDir = await readdir('python', moduleName);
     const pyFiles = pyDir
         .filter((value) =>
-            value[0].startsWith(block) &&
-            blocks.filterPyBlocks(value[0]))
+            blocks.filterPyBlocks(value[0]) && blocks.removeExt(value[0]) === block)
         .map(mapBlockToTreeItem('Implementation', ['python', moduleName]));
     const pyTestFiles = pyDir
         .filter((value) =>
-            blocks.filterBlockTests(value[0]) &&
-            value[0].endsWith(`${block}.py`))
+            RegExp(`^(qa_|test_)${block}\.py$`).test(value[0]))
         .map(mapBlockToTreeItem('Python Tests', ['python', moduleName]));
 
     const cppImplDir = await readdir('lib');
     const cppImplFiles = cppImplDir
         .filter((value) =>
-            value[0].startsWith(block) &&
-            (blocks.filterCppImplFiles(value[0]) || extname(value[0]) === '.h'))
+            (blocks.filterCppImplFiles(value[0]) || extname(value[0]) === '.h') &&
+            blocks.removeExt(value[0]) === block)
         .map((value) => {
             let item = mapBlockToTreeItem('Implementation', ['lib'])(value);
             item.label += extname(value[0]) === '.h' ? ' header' : ' source';
@@ -119,8 +123,7 @@ export async function getBlockFilesTree(block: string, baseUri: Uri, moduleName:
         });
     const cppTestFiles = cppImplDir
         .filter((value) =>
-            blocks.filterBlockTests(value[0]) &&
-            RegExp(`${block}\.(cc|cpp|cxx)`).test(value[0]))
+            RegExp(`^(qa_|test_)${block}\.(cc|cpp|cxx)$`).test(value[0]))
         .map(mapBlockToTreeItem('C++ Tests', ['lib']));
 
     return [...grcFiles, ...pyFiles, ...cppFiles, ...cppImplFiles, ...pyTestFiles, ...cppTestFiles];
